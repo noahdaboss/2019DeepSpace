98f62c30c81a3a34894972220e933ee0f04b9c64
LukeOxley <lukeoxley9@gmail.com>
2019-01-12 14:52:23 -0600

    fixed package somehow idk

--- 2019DeepSpace/src/main/java/frc/robot/AutoModeSelector.java
+++ 2019DeepSpace/src/main/java/frc/robot/AutoModeSelector.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.auto.AutoModeBase;
 import frc.robot.auto.modes.*;
 
-import JSONArray;
+import org.json.simple.JSONArray;
 
 import java.util.function.Supplier;
 
